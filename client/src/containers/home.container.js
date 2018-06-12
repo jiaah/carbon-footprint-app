3293016ca15a6a9374ff1e1ee4a9d6e2fb32e8bd
Jin827 <jiah.lee827@gmail.com>
2018-06-12 21:33:34 +0900

    combine home files

--- client/src/containers/home.container.js
+++ client/src/containers/home.container.js
@@ -2,18 +2,34 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 /* -- Import Components -- */
-import Home from './../components/home';
+import Nav from '../components/nav';
+import Footer from '../components/footer';
+import FlightCalculatorContainer from './calculator/flight.calculator.container';
+import Loader from '../components/loader';
+import ErrorBoundary from './error.boundary.container';
 
-
-const HomeContainer = ({ resultLoading }) => (
-
-  <Home
-    resultLoading={resultLoading}
-  />
-);
+const HomeContainer = ({ resultLoading }) => {
+  if (resultLoading) {
+    return (
+      <Loader />
+    );
+  }
+  return (
+    <div>
+      <Nav />
+      <ErrorBoundary>
+        <FlightCalculatorContainer />
+      </ErrorBoundary>
+      <Footer
+        link="© 2017 Flightnook- all rights reserved"
+      />
+    </div>
+  );
+};
 
 const mapStateToProps = state => ({
   resultLoading: state.carbonFootprint.loading,
 });
 
 export default connect(mapStateToProps)(HomeContainer);
+
