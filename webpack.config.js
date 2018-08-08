703cb3bb58ec3723a55aa66e6489287740ef733b
Jin827 <25231717+Jin827@users.noreply.github.com>
2018-08-08 19:11:36 +0900

    comment out Compression Plugin

--- webpack.config.js
+++ webpack.config.js
@@ -54,7 +54,7 @@ module.exports = (env) => {
           // }
         },
       }),
-      new CompressionPlugin()
+      // new CompressionPlugin()
     ],
     context: __dirname,
     devtool: isProduction ? 'source-map' : 'inline-source-map',
