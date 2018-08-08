e3237ae2c0617db22fba73e8a20fc4c517380330
Jin827 <25231717+Jin827@users.noreply.github.com>
2018-08-08 18:43:12 +0900

    delete compress options in uglifyJS

--- webpack.config.js
+++ webpack.config.js
@@ -42,17 +42,17 @@ module.exports = (env) => {
           output: {
             comments: false, // remove comments
           },
-          compress: {
-            unused: true,
-            dead_code: true, // big one--strip code that will never execute
-            warnings: false, // good for prod apps so users can't peek behind curtain
-            drop_debugger: true,
-            conditionals: true,
-            evaluate: true,
-            drop_console: true, // strips console statements
-            sequences: true,
-            booleans: true,
-          }
+          // compress: {
+          //   unused: true,
+          //   dead_code: true, // big one--strip code that will never execute
+          //   warnings: false, // good for prod apps so users can't peek behind curtain
+          //   drop_debugger: true,
+          //   conditionals: true,
+          //   evaluate: true,
+          //   drop_console: true, // strips console statements
+          //   sequences: true,
+          //   booleans: true,
+          // }
         },
       }),
       new CompressionPlugin()
